e6fa7facc300fafb1cbaece14d68fc698fa3c109
Yingzhe Reginald Fu <fuyingzhe@berkeley.edu>
2017-09-25 21:33:57 -0700

    Login user to the database

---
+++ server/src/controllers/AuthenticationController.js
@@ -0,0 +1,29 @@
+const {User} = require('../models')
+//
+// module.exports = {
+//   async register (req, res) {
+//     try {
+//       const user = await User.create(req.body)
+//       res.send(user.toJSON())
+//     } catch (err) {
+//       res.status(400).send({
+//         error: 'This email account is already in use'
+//       })
+//     }
+//   }
+// }
+module.exports = {
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson
+      })
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
+    }
+  }
+}
