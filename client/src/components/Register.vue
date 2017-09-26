6b63f356ce46b38c51c08033cd00a45fe945073e
Yingzhe Reginald Fu <fuyingzhe@berkeley.edu>
2017-09-25 18:30:30 -0700

    setup authentication server

---
+++ client/src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      value="email"
+      v-model="email"
+      placeholder="username@gmail.com" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      value="password"
+      v-model="password"
+      placeholder="******" />
+    </br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -2,7 +2,6 @@ import Api from './Api'
 
 export default {
   register (credentials) {
-    console.log(credentials)
     return Api().post('register', credentials)
   }
 }
