528910fc0c2b85da2a255f8004eba9ed28080f2f
Yingzhe Reginald Fu <fuyingzhe@berkeley.edu>
2017-09-26 16:56:22 -0700

    add login page ready to add webtoken

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,17 +1,29 @@
 <template>
   <v-toolbar fixed class="cyan">
     <v-toolbar-title class="mr-4">
-      Togg Dashboard
+      <span
+        class="home"
+        @click="navigateTo({name: 'root'})">
+        Togg Dashboard
+      </span>
+
     </v-toolbar-title>
     <v-toolbar-items>
       <v-btn flat dark>
         Browse
       </v-btn>
     </v-toolbar-items>
+
     <v-spacer></v-spacer>
+
     <v-toolbar-items>
-      <v-btn flat dark>
-        Sign Up
+      <v-btn flat dark
+        @click="navigateTo({name: 'login'})">
+          Login
+      </v-btn>
+      <v-btn flat dark
+        @click="navigateTo({name: 'register'})">
+          Sign Up
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
@@ -19,10 +31,20 @@
 
 <script>
 export default {
-
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .home {
+    cursor: pointer
+  }
+  .home:hover {
+    color: #DDD7D6
+  }
 </style>
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,5 +13,34 @@ module.exports = {
         error: 'This email account is already in use.'
       })
     }
+  },
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+      if (!user) {
+        res.status(403).send({
+          error: 'The login information is not found'
+        })
+      }
+      const isPasswordValid = password === user.password
+      if (!isPasswordValid) {
+        res.status(403).send({
+          error: 'The login information is not found'
+        })
+      }
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson
+      })
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to log in'
+      })
+    }
   }
 }
